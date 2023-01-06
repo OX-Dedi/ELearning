4656f9d54f93a94745cd628e1ba2e0a82cc559c7
OX-Dedi <96182960+OX-Dedi@users.noreply.github.com>
2023-01-06 15:53:07 +0700

    first commit

---
+++ application/views/manager/dashboard_view.php
@@ -0,0 +1,50 @@
+<!-- Content Header (Page header) -->
+<section class="content-header">
+	<h1>
+		<?php if(!empty($site_name)){ echo $site_name; } ?>
+	</h1>
+	<ol class="breadcrumb ">
+		<li><a href="<?php echo site_url(); ?>/manager"><i class="fa fa-dashboard"></i> Home</a></li>
+		<li class="active">Dashboard</li>
+	</ol>
+</section>
+
+<!-- Main content -->
+<section class="content">
+    <div class="box box-danger collapsed-box">
+        <div class="box-header  with-border">
+            <h3 class="box-title">Pesan</h3>
+            <div class="box-tools pull-right">
+                <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i></button>
+            </div><!-- /.box-tools -->
+        </div><!-- /.box-header -->
+        <div class="box-body">
+            Teruntuk rekan-rekan kerja di Unit Yanceloe Telkom University Saya Ucapkan Terimakasih yang sebesar-besarnya.
+        </div><!-- /.box-body -->
+    </div><!-- /.box -->
+	<div class="callout callout-danger">
+    	<h4>Informasi</h4>
+        <p>Tolong membayar UKT Tepat waktu ya...</p>
+    </div>  
+    <div class="box box-danger box-solid">
+		<div class="box-header with-border">
+        	<h3 class="box-title">Petunjuk Penggunaan</h3>
+        </div><!-- /.box-header -->
+        <div class="box-body">
+        	<dl>
+        		<dt>Data Modul</dt>
+                <dd>
+                	Kelompok Data Modul digunakan untuk menambah modul, topik, dan soal. Serta digunakan untuk mengatur file dengan memanfaatkan File Manager.
+                	<ol>
+                		<li>Modul</li>
+                		<li>Topik</li>
+                		<li>Soal</li>
+                		<li>Import Soal Word</li>
+						<li>Import Soal Spreadsheet</li>
+                		<li>Daftar Soal</li>
+                		<li>File Manager</li>
+                	</ol>
+                </dd>
+            </div><!-- /.box-body -->
+            </div><!-- /.box -->
+</section><!-- /.content -->
